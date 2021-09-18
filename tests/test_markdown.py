e4244c28d023f0ddaceb91710ff70cf2f423895f
Tom Lockwood <31057261+tomlockwood@users.noreply.github.com>
2021-09-18 13:19:00 +0000

    Test update

--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -5,10 +5,16 @@ from desword.parsing.md import CustomMarkdownParser
 
 @pytest.fixture
 def markdown_parser():
-    return CustomMarkdownParser('./generic/path')
+    return CustomMarkdownParser('./generic/path/')
 
 
-def test_links_generation(markdown_parser):
-    html, links = markdown_parser.generate_html_and_links('Hi')
+def test_regular_generation(markdown_parser):
+    html, links = markdown_parser.generate_html_and_links('# Hi')
     assert len(links) == 0
-    assert html == '<p>Hi</p>'
+    assert html == '<h1>Hi</h1>'
+
+
+def test_link_generation(markdown_parser):
+    _, links = markdown_parser.generate_html_and_links('# Hi\n[bbb](tengu)')
+    assert len(links) == 1
+    assert links[0] == {'href': './generic/path/tengu.html', 'text': 'bbb'}
