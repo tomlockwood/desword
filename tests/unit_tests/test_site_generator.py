44a128d7724eb66ce603ac14adc5b34bd5723668
Tom Lockwood <31057261+tomlockwood@users.noreply.github.com>
2021-09-22 03:51:43 +0000

    More site generator testing

--- tests/unit_tests/test_site_generator.py
+++ tests/unit_tests/test_site_generator.py
@@ -21,6 +21,20 @@ def test_site_generator_no_markdown_file(site_generator):
 
 
 def test_add_page_to_graph(site_generator):
-    site_generator.file_graph = {"a": {"lines": " # Hi"}}
+    site_generator.file_graph = {
+        "a": {"lines": " # Hi"}}
     site_generator.add_pages_to_graph()
     assert site_generator.file_graph['a'].get('page')
+
+
+def test_record_link_edges(site_generator):
+    site_generator.file_graph = {
+        "a": {"lines": " # Hi\n Augh [hi](hello/butt)"}, "b": {"lines": "[aaa](a)"}}
+    site_generator.add_pages_to_graph()
+    assert site_generator.file_graph['a'].get('page')
+    site_generator.record_link_edges()
+    a_page = site_generator.file_graph['a']['page']
+    assert len(a_page.backlinks) == 1
+    link = a_page.links[0]
+    assert link['rel'] == 'hello/butt'
+    assert link['text'] == 'hi'
