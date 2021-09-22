a08574bbd23d78e7fb3e85785e26bf228a1a387e
Tom Lockwood <31057261+tomlockwood@users.noreply.github.com>
2021-09-22 05:09:01 +0000

    Serve Files

--- example.sh
+++ example.sh
@@ -1,1 +1,3 @@
-python desword/main.py tests/example/ tests/tmp/ "www.example.com/butt/"
+python desword/main.py tests/example/ tests/tmp/ https://${CODESPACE_NAME}-8000.githubpreview.dev/
+echo "https://${CODESPACE_NAME}-8000.githubpreview.dev/"
+python -m http.server -d tests/tmp/ 8000
