694d02bc0bcb3d3f59e9738c4ab09bfc63b58b04
Tom Lockwood <31057261+tomlockwood@users.noreply.github.com>
2021-10-01 12:55:52 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='desword',
+    version='0.0.1',
+    author='Tom Lockwood',
+    author_email='tom@lockwood.dev',
+    description="A markdown worldbuilder's assistant",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/tomlockwood/desword',
+    project_urls={
+        "Bug Tracker": "https://github.com/tomlockwood/desword/issues"
+    },
+    packages=['desword'],
+    install_requires=['Markdown==3.3.4'],
+)
